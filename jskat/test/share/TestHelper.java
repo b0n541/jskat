3473985e328cf305767ecd98928bb03418f15e01
b0n541 <b0n541@github>
2007-09-28 08:19:01 +0000

    Added null ouvert game

--- jskat/test/share/TestHelper.java
+++ jskat/test/share/TestHelper.java
@@ -131,12 +131,19 @@ public class TestHelper {
 			dealtCards[2] = convertCardsToHashSet(buildDeck("J-C,J-S,J-H,J-D,A-C,T-C,K-C,A-S,T-S,K-S"));
 			dealtCards[3] = convertCardsToHashSet(buildDeck("8-D,7-D"));
 			break;
-		// a distribution for playing a jungfrau ramsch
+			// a distribution for playing a jungfrau ramsch
 		case 1:
 			dealtCards[0] = convertCardsToHashSet(buildDeck("A-C,T-S,A-S,Q-H,K-H,T-H,A-H,9-D,Q-D,K-D"));
 			dealtCards[1] = convertCardsToHashSet(buildDeck("J-C,J-S,J-H,J-D,9-H,K-S,7-D,8-D,K-C,T-C"));
 			dealtCards[2] = convertCardsToHashSet(buildDeck("Q-C,9-C,8-C,7-C,Q-S,9-S,8-S,7-S,7-H,8-H"));
 			dealtCards[3] = convertCardsToHashSet(buildDeck("A-D,T-D"));
+			break;
+		// a distribution for playing a null ouvert
+		case 2:
+			dealtCards[0] = convertCardsToHashSet(buildDeck("J-H,Q-H,K-H,A-H,7-D,8-D,9-D,T-D,J-D,Q-D"));
+			dealtCards[1] = convertCardsToHashSet(buildDeck("9-S,T-S,J-S,Q-S,K-S,A-S,7-H,8-H,9-H,T-H"));
+			dealtCards[2] = convertCardsToHashSet(buildDeck("7-C,8-C,9-C,T-C,J-C,Q-C,K-C,A-C,7-S,8-S"));
+			dealtCards[3] = convertCardsToHashSet(buildDeck("K-D,A-D"));
 			break;
 		default:
 			throw new IllegalArgumentException("Illegal predefined cardset!");
