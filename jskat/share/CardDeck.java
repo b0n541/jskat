b92f3d80d9b70cfdba45bddca1f820a72b4e319d
b0n541 <b0n541@github>
2008-02-05 23:28:36 +0000

    changes due to refactoring of Card class

--- jskat/share/CardDeck.java
+++ jskat/share/CardDeck.java
@@ -14,29 +14,32 @@ package jskat.share;
 import jskat.share.SkatConstants;
 
 /**
- * A carddeck for skat
+ * CardDeck class representing a complete card deck for Skat
  *
  * @author  Jan Schäfer <jan.schaefer@b0n541.net>
  */
 public class CardDeck extends CardVector {
     
-    /** Creates a new instance of CardDeck */
+    /** 
+     * Creates a new instance of CardDeck
+     */
     public CardDeck() {
         
         // Creates cards for every suit
-        for (int i = SkatConstants.CLUBS; i <= SkatConstants.DIAMONDS; i++) {
+        for (SkatConstants.Suits currSuit : SkatConstants.Suits.values()) {
             // and every value
-            for (int j = SkatConstants.SEVEN; j <= SkatConstants.JACK; j++) {
+            for (SkatConstants.Ranks currRank : SkatConstants.Ranks.values()) {
                 
-                add(new Card(i, j));
+                add(new Card(currSuit, currRank));
             }
         }
     }
     
-    /** Shuffles the CardDeck */
+    /** 
+     * Shuffles the CardDeck 
+     */
     public void shuffle() {
         
-        // XXX Implementation of some more human like shuffling methods
         // Simple random shuffling
         CardVector shuffledCardDeck = new CardVector();
         
@@ -49,7 +52,5 @@ public class CardDeck extends CardVector {
             
             add(shuffledCardDeck.remove(0));
         }
-
-        // XXX after shuffling, let one player cut the card deck
     }
 }
