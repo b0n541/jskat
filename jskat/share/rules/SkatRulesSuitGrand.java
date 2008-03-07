3b6150d86fbac6651c9af468d77da0d13cb50749
b0n541 <b0n541@github>
2008-03-07 09:32:35 +0000

    cosmetics

--- jskat/share/rules/SkatRulesSuitGrand.java
+++ jskat/share/rules/SkatRulesSuitGrand.java
@@ -22,49 +22,6 @@ import jskat.share.SkatConstants;
 public abstract class SkatRulesSuitGrand implements SkatRules {
 
 	/**
-	 * @see jskat.share.rules.SkatRules#isGameWon(jskat.data.SkatGameData)
-	 */
-	public boolean isGameWon(SkatGameData gameData) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/**
-	 * @see jskat.share.rules.SkatRules#isSchneider(jskat.data.SkatGameData)
-	 */
-	public boolean isSchneider(SkatGameData gameData) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/**
-	 * @see jskat.share.rules.SkatRules#isSchwarz(jskat.data.SkatGameData)
-	 */
-	public boolean isSchwarz(SkatGameData gameData) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	/**
-	 * @see jskat.share.rules.SkatRules#isDurchMarsch(int, jskat.data.SkatGameData)
-	 */
-	public boolean isDurchMarsch(int playerID, SkatGameData gameData) {
-		return false;
-	}
-
-	/**
-	 * @see jskat.share.rules.SkatRules#isJungFrau(int, jskat.data.SkatGameData)
-	 */
-	public boolean isJungFrau(int playerID, SkatGameData gameData) {
-		return false;
-	}
-
-	/**
-	 * @see jskat.share.rules.SkatRules#getGameResult(jskat.data.SkatGameData)
-	 */
-	public abstract int getGameResult(SkatGameData gameData);
-
-	/**
 	 * @see jskat.share.rules.SkatRules#isCardAllowed(jskat.share.Card, jskat.share.CardVector, jskat.share.Card, jskat.share.SkatConstants.Suits)
 	 */
 	public abstract boolean isCardAllowed(Card card, CardVector hand, Card initialCard,
@@ -74,4 +31,49 @@ public abstract class SkatRulesSuitGrand implements SkatRules {
 	 * @see jskat.share.rules.SkatRules#isCardBeatsCard(jskat.share.Card, jskat.share.Card, jskat.share.Card, jskat.share.SkatConstants.Suits)
 	 */
 	public abstract boolean isCardBeatsCard(Card card, Card cardToBeat, Card initialCard, SkatConstants.Suits trump);
+
+	/**
+	 * @see jskat.share.rules.SkatRules#getGameResult(jskat.data.SkatGameData)
+	 */
+	public abstract int getGameResult(SkatGameData gameData);
+
+	/**
+	 * @see jskat.share.rules.SkatRules#isGameWon(jskat.data.SkatGameData)
+	 */
+	public boolean isGameWon(SkatGameData gameData) {
+		// TODO implement it
+		return false;
+	}
+
+	/**
+	 * @see jskat.share.rules.SkatRules#isSchneider(jskat.data.SkatGameData)
+	 */
+	public boolean isSchneider(SkatGameData gameData) {
+		// TODO implement it
+		return false;
+	}
+
+	/**
+	 * @see jskat.share.rules.SkatRules#isSchwarz(jskat.data.SkatGameData)
+	 */
+	public boolean isSchwarz(SkatGameData gameData) {
+		// TODO implement it
+		return false;
+	}
+
+	/**
+	 * @see jskat.share.rules.SkatRules#isDurchMarsch(int, jskat.data.SkatGameData)
+	 */
+	public boolean isDurchMarsch(int playerID, SkatGameData gameData) {
+		// there is no durchmarsch in suit or grand games
+		return false;
+	}
+
+	/**
+	 * @see jskat.share.rules.SkatRules#isJungFrau(int, jskat.data.SkatGameData)
+	 */
+	public boolean isJungFrau(int playerID, SkatGameData gameData) {
+		// there is no jungfrau in suit or grand games
+		return false;
+	}
 }
