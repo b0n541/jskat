fb5e7918cea88307c2f6c0d8963848de27879b81
b0n541 <b0n541@github>
2008-08-05 08:29:51 +0000

    removed @Override annotations

--- jskat/player/AIPlayerNN/AIPlayerNN.java
+++ jskat/player/AIPlayerNN/AIPlayerNN.java
@@ -15,80 +15,68 @@ public class AIPlayerNN extends AbstractJSkatPlayer implements JSkatPlayer {
 
 	private Log log = LogFactory.getLog(AIPlayerNN.class);
 	
-	@Override
+
 	public GameAnnouncement announceGame() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public boolean bidMore(int currBidValue) {
 		// TODO Auto-generated method stub
 		log.debug("ASDFJKLÖ");
 		return false;
 	}
 
-	@Override
 	public void cardPlayed(Card card) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void discloseOuvertCards(CardVector ouvertCards) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public boolean isAIPlayer() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean isHumanPlayer() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean lookIntoSkat(boolean isRamsch) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public Card playCard(CardVector trick) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Card removeCard(Suits suit, Ranks rank) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void setUpBidding(int initialForehandPlayer) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void showTrick(CardVector trick, int trickWinner) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void takeRamschSkat(CardVector skat, boolean jacksAllowed) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	public void takeSkat(CardVector skat) {
 		// TODO Auto-generated method stub
 
