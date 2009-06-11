2cb0dfd2c661ba621cb9fc871ebcce194918a374
mjluzius <mjluzius@sourceforge>
2009-06-11 05:45:02 +0000

    fixed Nimbus L&F on non-Sun-JDK6

--- de/jskat/gui/LookAndFeelSetter.java
+++ de/jskat/gui/LookAndFeelSetter.java
@@ -2,31 +2,40 @@ package de.jskat.gui;
 
 import java.awt.Color;
 
+import javax.swing.LookAndFeel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.UIManager.LookAndFeelInfo;
 
-import com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class LookAndFeelSetter {
+
+	private static Log log = LogFactory.getLog(LookAndFeelSetter.class);
 	
 	public static void setLookAndFeel() {
 		
 		try {
 			
-			NimbusLookAndFeel nlaf = new NimbusLookAndFeel();
+			LookAndFeel laf = (LookAndFeel) Class.forName("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel").newInstance();
 			
-			nlaf.getDefaults().put("control", new Color(226, 217, 202));
-			nlaf.getDefaults().put("text", new Color(0, 0, 0));
-			nlaf.getDefaults().put("nimbusFocus", new Color(255, 245, 193));
-			nlaf.getDefaults().put("nimbusLightBackground", new Color(241, 238, 229));
-			nlaf.getDefaults().put("nimbusBase", new Color(96, 65, 34));
+			laf.getDefaults().put("control", new Color(226, 217, 202));
+			laf.getDefaults().put("text", new Color(0, 0, 0));
+			laf.getDefaults().put("nimbusFocus", new Color(255, 245, 193));
+			laf.getDefaults().put("nimbusLightBackground", new Color(241, 238, 229));
+			laf.getDefaults().put("nimbusBase", new Color(96, 65, 34));
 			
-			UIManager.setLookAndFeel(nlaf);
+			UIManager.setLookAndFeel(laf);
+			log.debug("NimbusLookAndFeel successfully applied...");
 			
 		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.debug(e);
+		} catch (InstantiationException e) {
+			log.debug(e);
+		} catch (IllegalAccessException e) {
+			log.debug(e);
+		} catch (ClassNotFoundException e) {
+			log.info("NimbusLookAndFeel not found (probably not installed/wrong JDK)!");
 		}
 	}
 }
