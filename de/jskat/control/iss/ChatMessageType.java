46ff47505c4183ec9a421f4c87107c7a364ace63
b0n541 <b0n541@github>
2009-06-15 17:47:44 +0000

    cosmetics

--- de/jskat/control/iss/ChatMessageType.java
+++ de/jskat/control/iss/ChatMessageType.java
@@ -11,9 +11,21 @@ Released: @ReleaseDate@
 
 package de.jskat.control.iss;
 
+/**
+ * All chat message types
+ */
 public enum ChatMessageType {
 
+	/**
+	 * Chat message in ISS lobby
+	 */
 	LOBBY,
+	/**
+	 * Chat message at ISS table
+	 */
 	TABLE,
+	/**
+	 * Chat message from user to user
+	 */
 	USER
 }
