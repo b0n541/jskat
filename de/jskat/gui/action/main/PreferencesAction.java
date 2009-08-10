8606dff999c493c68102a8f1df9281a547570588
b0n541 <b0n541@github>
2009-08-10 15:00:17 +0000

    Preferences dialog recreated nothing is saved a.t.m.

---
+++ de/jskat/gui/action/main/PreferencesAction.java
@@ -0,0 +1,55 @@
+/*
+
+@ShortLicense@
+
+Authors: @JS@
+         @MJL@
+
+Released: @ReleaseDate@
+
+ */
+
+package de.jskat.gui.action.main;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+
+import de.jskat.control.JSkatMaster;
+import de.jskat.gui.action.AbstractJSkatAction;
+import de.jskat.gui.img.JSkatGraphicRepository;
+
+/**
+ * Implements the action for showing the preferences dialog
+ */
+public class PreferencesAction extends AbstractJSkatAction {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see AbstractJSkatAction#AbstractJSkatAction(JSkatMaster)
+	 */
+	public PreferencesAction(JSkatMaster controller, JSkatGraphicRepository bitmaps) {
+		
+		super(controller);
+		
+		putValue(Action.NAME, "Preferences");
+		putValue(Action.SMALL_ICON, new ImageIcon(bitmaps.getIconImage(
+				JSkatGraphicRepository.Icon.PREFERENCES, 
+				JSkatGraphicRepository.IconSize.SMALL)));
+		putValue(Action.LARGE_ICON_KEY, new ImageIcon(bitmaps.getIconImage(
+				JSkatGraphicRepository.Icon.PREFERENCES, 
+				JSkatGraphicRepository.IconSize.BIG)));
+	}
+	
+	/**
+	 * @see AbstractAction#actionPerformed(ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+		this.jskat.showPreferences();
+	}
+}
