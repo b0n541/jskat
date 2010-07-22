b70b88714e4cae48a27049384a322841c924567f
b0n541 <b0n541@github>
2010-07-22 18:30:51 +0000

    Javadoc

--- de/jskat/control/iss/OutputChannel.java
+++ de/jskat/control/iss/OutputChannel.java
@@ -17,34 +17,34 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Handles all incoming messages from ISS
+ * Handles all outgoing messages to ISS
  */
 class OutputChannel {
 
-    private static Log log = LogFactory.getLog(OutputChannel.class);
+	private static Log log = LogFactory.getLog(OutputChannel.class);
 
-    /**
-     * Constructor
-     * 
-     * @param newOutput
-     *            Input stream from ISS
-     */
-    OutputChannel(PrintWriter newOutput) {
+	/**
+	 * Constructor
+	 * 
+	 * @param newOutput
+	 *            Input stream from ISS
+	 */
+	OutputChannel(PrintWriter newOutput) {
 
-	this.output = newOutput;
-    }
+		this.output = newOutput;
+	}
 
-    /**
-     * Sends a message to ISS
-     * 
-     * @param message
-     *            Message text
-     */
-    void send(String message) {
+	/**
+	 * Sends a message to ISS
+	 * 
+	 * @param message
+	 *            Message text
+	 */
+	void send(String message) {
 
-	log.debug("ISS <--|    " + message); //$NON-NLS-1$
-	this.output.println(message);
-    }
+		log.debug("ISS <--|    " + message); //$NON-NLS-1$
+		this.output.println(message);
+	}
 
-    private PrintWriter output;
+	private PrintWriter output;
 }
