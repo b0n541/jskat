dfa82f8568deec6c70cc51859161e22f9d1f7325
b0n541 <b0n541@github>
2011-06-04 12:58:45 +0000

    cards are removed during trick playing on ISS games

--- main/java/org/jskat/gui/table/CardPanel.java
+++ main/java/org/jskat/gui/table/CardPanel.java
@@ -19,7 +19,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 package org.jskat.gui.table;
 
 import java.awt.Graphics;
@@ -49,7 +48,6 @@ import org.jskat.util.CardList;
 import org.jskat.util.GameType;
 import org.jskat.util.Rank;
 import org.jskat.util.Suit;
-
 
 /**
  * Panel for showing cards on a hand
@@ -142,7 +140,11 @@ class CardPanel extends JPanel {
 
 	protected void removeCard(Card cardToRemove) {
 
-		cards.remove(cardToRemove);
+		if (cards.contains(cardToRemove)) {
+			cards.remove(cardToRemove);
+		} else {
+			cards.remove(cards.size() - 1);
+		}
 		repaint();
 	}
 
@@ -164,12 +166,15 @@ class CardPanel extends JPanel {
 
 		// rendering hints
 		Graphics2D g2D = (Graphics2D) g;
-		g2D.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2D.setRenderingHint(RenderingHints.KEY_RENDERING,
+				RenderingHints.VALUE_RENDER_QUALITY);
+		g2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
 
 		// calculate card gap
 		int panelWidth = getWidth();
-		int cardWidth = bitmaps.getCardImage(Suit.CLUBS, Rank.JACK).getWidth(this);
+		int cardWidth = bitmaps.getCardImage(Suit.CLUBS, Rank.JACK).getWidth(
+				this);
 		int cardGap = cardWidth;
 		if (cards.size() * cardGap > panelWidth) {
 
@@ -255,12 +260,14 @@ class CardPanel extends JPanel {
 
 		log.debug("Card panel clicked at: " + xPosition + " x " + yPosition); //$NON-NLS-1$ //$NON-NLS-2$
 
-		if (xPosition > -1 && xPosition < getWidth() && yPosition > -1 && yPosition < getHeight()) {
+		if (xPosition > -1 && xPosition < getWidth() && yPosition > -1
+				&& yPosition < getHeight()) {
 
 			log.debug("Mouse button release inside panel"); //$NON-NLS-1$
 
 			// get card
-			double cardWidth = bitmaps.getCardImage(Suit.CLUBS, Rank.JACK).getWidth(this);
+			double cardWidth = bitmaps.getCardImage(Suit.CLUBS, Rank.JACK)
+					.getWidth(this);
 
 			int cardIndex = -1;
 			if (cards.size() > 0) {
@@ -272,7 +279,8 @@ class CardPanel extends JPanel {
 				} else {
 					double distanceBetweenCards = cardWidth;
 					if (cards.size() > 1) {
-						distanceBetweenCards = (getWidth() - cardWidth) / (cards.size() - 1.0);
+						distanceBetweenCards = (getWidth() - cardWidth)
+								/ (cards.size() - 1.0);
 					}
 
 					if (cardWidth > distanceBetweenCards) {
@@ -309,7 +317,8 @@ class CardPanel extends JPanel {
 
 				if (parent instanceof DiscardPanel) {
 					// card panel in discard panel was clicked
-					action = getActionMap().get(JSkatAction.TAKE_CARD_FROM_SKAT);
+					action = getActionMap()
+							.get(JSkatAction.TAKE_CARD_FROM_SKAT);
 				} else if (parent instanceof JSkatUserPanel) {
 					// card panel in player panel was clicked
 
@@ -317,7 +326,8 @@ class CardPanel extends JPanel {
 
 					if (state == GameState.DISCARDING) {
 						// discarding phase
-						action = getActionMap().get(JSkatAction.PUT_CARD_INTO_SKAT);
+						action = getActionMap().get(
+								JSkatAction.PUT_CARD_INTO_SKAT);
 					} else if (state == GameState.TRICK_PLAYING) {
 						// trick playing phase
 						action = getActionMap().get(JSkatAction.PLAY_CARD);
@@ -329,9 +339,11 @@ class CardPanel extends JPanel {
 
 				if (action != null) {
 
-					action.actionPerformed(new ActionEvent(Card.getCardFromString(card.getSuit().shortString()
-							+ card.getRank().shortString()), ActionEvent.ACTION_PERFORMED, (String) action
-							.getValue(Action.ACTION_COMMAND_KEY)));
+					action.actionPerformed(new ActionEvent(Card
+							.getCardFromString(card.getSuit().shortString()
+									+ card.getRank().shortString()),
+							ActionEvent.ACTION_PERFORMED, (String) action
+									.getValue(Action.ACTION_COMMAND_KEY)));
 				} else {
 
 					log.debug("Action is null"); //$NON-NLS-1$
