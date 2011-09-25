5079d57875577883277a05472bd02de0e393d4bf
b0n541 <b0n541@github>
2011-09-25 08:34:59 +0000

    methods not needed, use getTrumpSuit() instead

--- main/java/org/jskat/util/GameType.java
+++ main/java/org/jskat/util/GameType.java
@@ -27,77 +27,80 @@ public enum GameType {
 	/**
 	 * Suit game with Clubs as trump
 	 */
-	CLUBS { 
+	CLUBS {
 		@Override
-		public Suit getTrumpSuit() { return Suit.CLUBS; }
-	}, 
+		public Suit getTrumpSuit() {
+			return Suit.CLUBS;
+		}
+	},
 	/**
 	 * Suit game with Spades as trump
 	 */
-	SPADES { 
+	SPADES {
 		@Override
-		public Suit getTrumpSuit() { return Suit.SPADES; }
-	}, 
+		public Suit getTrumpSuit() {
+			return Suit.SPADES;
+		}
+	},
 	/**
 	 * Suit game with Hearts as trump
 	 */
-	HEARTS { 
+	HEARTS {
 		@Override
-		public Suit getTrumpSuit() { return Suit.HEARTS; }
-	}, 
+		public Suit getTrumpSuit() {
+			return Suit.HEARTS;
+		}
+	},
 	/**
 	 * Suit game with Diamonds as trump
 	 */
-	DIAMONDS { 
+	DIAMONDS {
 		@Override
-		public Suit getTrumpSuit() { return Suit.DIAMONDS; }
-	}, 
+		public Suit getTrumpSuit() {
+			return Suit.DIAMONDS;
+		}
+	},
 	/**
 	 * Grand game
 	 */
-	GRAND { 
+	GRAND {
 		@Override
-		public Suit getTrumpSuit() { return null; }
-	}, 
+		public Suit getTrumpSuit() {
+			return null;
+		}
+	},
 	/**
 	 * Null game
 	 */
-	NULL { 
+	NULL {
 		@Override
-		public Suit getTrumpSuit() { return null; }
-	}, 
+		public Suit getTrumpSuit() {
+			return null;
+		}
+	},
 	/**
 	 * Ramsch game, this is not playable under ISPA rules
 	 */
 	RAMSCH {
 		@Override
-		public Suit getTrumpSuit() { return null; }
+		public Suit getTrumpSuit() {
+			return null;
+		}
 	},
 	/**
 	 * Passed in game, no player did a bid
 	 */
-	PASSED_IN { 
+	PASSED_IN {
 		@Override
-		public Suit getTrumpSuit() { return null; }
+		public Suit getTrumpSuit() {
+			return null;
+		}
 	};
-	
+
 	/**
 	 * Gets the trump suit
 	 * 
 	 * @return Trump suit, null if there is no trump suit
 	 */
 	public abstract Suit getTrumpSuit();
-
-	public boolean equals(Suit s) {
-		return s.name().equalsIgnoreCase(this.name());
-	}
-	
-	public Suit asSuit() {
-		try {
-			return Suit.valueOf(this.name());
-		} catch (Exception e) {
-			return null;
-		}
-	}
-	
 }
