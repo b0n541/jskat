2e6f2c162442ab7a3620eda37f5da13b8de205d9
b0n541 <b0n541@github>
2011-11-14 17:04:39 +0000

    0000027: Re-enabled bidding

--- main/java/org/jskat/ai/algorithmic/BidEvaluator.java
+++ main/java/org/jskat/ai/algorithmic/BidEvaluator.java
@@ -28,13 +28,12 @@ import org.jskat.util.GameType;
 import org.jskat.util.SkatConstants;
 import org.jskat.util.Suit;
 
-
 /**
- * The BidEvaluator is the class to generate the acceptable bid value and to decide, which game to play after
- * discarding the Skat.
- *  
+ * The BidEvaluator is the class to generate the acceptable bid value and to
+ * decide, which game to play after discarding the Skat.
+ * 
  * @author Markus J. Luzius <markus@luzius.de>
- *
+ * 
  */
 class BidEvaluator {
 
@@ -46,44 +45,49 @@ class BidEvaluator {
 	private int maxBid = -1;
 	private GameType suggestedGameType = null;
 
-    /** default constructor
-	 * @param cards hand of the player
+	/**
+	 * default constructor
+	 * 
+	 * @param cards
+	 *            hand of the player
 	 */
 	BidEvaluator(CardList cards) {
-	    log.debug("Checking out what to bid with ["+cards+"]"+cards.dumpFlag());
-	    if(cards.size()<10) {
-	    	log.warn("Not enough cards for bid evaluation!");
-	    	return;
-	    }
+		log.debug("Checking out what to bid with [" + cards + "]" + cards.dumpFlag());
+		if (cards.size() < 10) {
+			log.warn("Not enough cards for bid evaluation!");
+			return;
+		}
 
 		Suit mostFrequentSuitColor;
 		int mostFrequentSuitColorValue = 0;
 		int multiplier = Helper.getMultiplier(cards);
-        
+
 		mostFrequentSuitColor = cards.getMostFrequentSuit();
 		int noOfTrumps = cards.getSuitCount(mostFrequentSuitColor, false);
 		int noOfJacks = Helper.countJacks(cards);
-        
+
 		if (mostFrequentSuitColor == Suit.CLUBS) {
 			mostFrequentSuitColorValue = SkatConstants.getGameBaseValue(GameType.CLUBS, false, false);
-		}
-		else if (mostFrequentSuitColor == Suit.SPADES) {
+		} else if (mostFrequentSuitColor == Suit.SPADES) {
 			mostFrequentSuitColorValue = SkatConstants.getGameBaseValue(GameType.SPADES, false, false);
-		}
-		else if (mostFrequentSuitColor == Suit.HEARTS) {
+		} else if (mostFrequentSuitColor == Suit.HEARTS) {
 			mostFrequentSuitColorValue = SkatConstants.getGameBaseValue(GameType.HEARTS, false, false);
-		}
-		else if (mostFrequentSuitColor == Suit.DIAMONDS) {
+		} else if (mostFrequentSuitColor == Suit.DIAMONDS) {
 			mostFrequentSuitColorValue = SkatConstants.getGameBaseValue(GameType.DIAMONDS, false, false);
 		}
 		maxBid = mostFrequentSuitColorValue * multiplier;
 
-		// but I will only play, if I have at least 1 jack and 4 color cards or 2 jacks and 3 color cards
-		if (noOfJacks < 3 && noOfTrumps < 4) maxBid = 0;
-		else if (noOfJacks < 2 && noOfTrumps < 5) maxBid = 0;
-		else if (noOfJacks < 1 && noOfTrumps < 6) maxBid = 0;
-		else if ((Helper.getJacks(cards)&3)==0 && noOfTrumps < 5) maxBid = 0;
-        
+		// but I will only play, if I have at least 1 jack and 4 color cards or
+		// 2 jacks and 3 color cards
+		if (noOfJacks < 3 && noOfTrumps < 4)
+			maxBid = 0;
+		else if (noOfJacks < 2 && noOfTrumps < 5)
+			maxBid = 0;
+		else if (noOfJacks < 1 && noOfTrumps < 6)
+			maxBid = 0;
+		else if ((Helper.getJacks(cards) & 3) == 0 && noOfTrumps < 5)
+			maxBid = 0;
+
 		if (maxBid > 0) {
 			switch (mostFrequentSuitColor) {
 			case CLUBS:
@@ -100,23 +104,25 @@ class BidEvaluator {
 				break;
 			}
 		}
-		
-		
-		// FIXME (markus, 01.11.11) remove - only added for ramsch testing
-		maxBid = 0;
-		
-		log.debug("I will bid until " + maxBid +" (m="+multiplier+"x) - I have "+noOfJacks+" Jacks and an additional "+noOfTrumps+" trump cards in suit "+mostFrequentSuitColor);
+
+		log.debug("I will bid until " + maxBid + " (m=" + multiplier + "x) - I have " + noOfJacks
+				+ " Jacks and an additional " + noOfTrumps + " trump cards in suit " + mostFrequentSuitColor);
 	}
 
-	/** Gets the maximum bid value of the player
+	/**
+	 * Gets the maximum bid value of the player
+	 * 
 	 * @return maximum bid value
 	 */
 	int getMaxBid() {
 		return maxBid;
 	}
 
-	/** tells the AI player whether to pick up the skat or not
-	 * @return true, if the skat should be picked up;<br>false, for a hand game
+	/**
+	 * tells the AI player whether to pick up the skat or not
+	 * 
+	 * @return true, if the skat should be picked up;<br>
+	 *         false, for a hand game
 	 */
 	boolean pickUpSkat() {
 		return true;
@@ -125,5 +131,5 @@ class BidEvaluator {
 	GameType getSuggestedGameType() {
 		return suggestedGameType;
 	}
-	
+
 }
