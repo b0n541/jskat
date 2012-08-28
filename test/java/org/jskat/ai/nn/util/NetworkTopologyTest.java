0bd6a6b3992039c31616cb4ac7bb29f18b41de81
b0n541 <b0n541@github>
2012-08-28 17:27:11 +0000

    Preparations for release of version 0.11

--- test/java/org/jskat/ai/nn/util/NetworkTopologyTest.java
+++ test/java/org/jskat/ai/nn/util/NetworkTopologyTest.java
@@ -1,3 +1,22 @@
+/**
+ * JSkat - A skat program written in Java
+ * by Jan Schäfer and Markus J. Luzius
+ *
+ * Version 0.11.0
+ * Copyright (C) 2012-08-28
+ *
+ * Licensed under the Apache License, Version 2.0. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.jskat.ai.nn.util;
 
 import static org.junit.Assert.assertEquals;
