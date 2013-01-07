3a62dd392e2bd71b7a3e2a4377c4c3b8694f76bd
Daniel L <daniel.loreck@gmail.com>
2013-01-07 14:24:21 +0100

    neu Algorithm-KI-Klassen

--- main/java/org/jskat/ai/newalgorithm/AbstractAlgorithmAI.java
+++ main/java/org/jskat/ai/newalgorithm/AbstractAlgorithmAI.java
@@ -1,21 +1,22 @@
 /**
  * JSkat - A skat program written in Java
- * by Jan Schäfer, Markus J. Luzius and Daniel Loreck
+ * by Jan SchÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â¤fer and Markus J. Luzius
  *
- * Version 0.11.0
- * Copyright (C) 2012-08-28
+ * Version 0.11.0-SNAPSHOT
+ * Copyright (C) 2012-03-13
  *
- * Licensed under the Apache License, Version 2.0. You may
- * obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jskat.ai.newalgorithm;
 
@@ -223,5 +224,27 @@ public abstract class AbstractAlgorithmAI {
 		while(!pCards.get(counter).beats(pGameType, tCardToBeat))
 			counter--;
 		return pCards.get(counter);
+	}
+	
+	protected static CardList getPossibleMaxValueCards(CardList pCards, int pMaxCardValue) {
+		CardList possibleCards				= new CardList();
+		
+		for(Suit lSuit : Suit.values()) {
+			int  suitCount			= pCards.getSuitCount(lSuit, false);
+			Card possibleHighCard	= pCards.get(pCards.getFirstIndexOfSuit(lSuit, false));
+			Card possibleLowCard	= pCards.get(pCards.getLastIndexOfSuit(lSuit, false));
+			
+			if(suitCount > 0
+					&& possibleHighCard == possibleLowCard
+					&& possibleLowCard.getPoints() <= pMaxCardValue) {
+				possibleCards.add(possibleLowCard);
+			}
+			else if(suitCount > 2
+					&& possibleLowCard.getPoints() <= pMaxCardValue) {
+				possibleCards.add(possibleLowCard);
+			}
+		}
+		
+		return possibleCards;
 	}
 }
