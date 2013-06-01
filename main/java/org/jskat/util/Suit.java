e52fcc8aa0acf6949fc7bb0c42ff5089237e540c
b0n541 <b0n541@github>
2013-06-01 18:22:58 +0200

    removed unused equals method

--- main/java/org/jskat/util/Suit.java
+++ main/java/org/jskat/util/Suit.java
@@ -43,13 +43,13 @@ public enum Suit {
 	private final String shortString;
 	private final String longString;
 	private final int sortOrder;
-	
+
 	private Suit(String s, String l, int order) {
 		shortString = s;
 		longString = l;
 		sortOrder = order;
 	}
-	
+
 	/**
 	 * Gets a short string representation of the constant
 	 * 
@@ -76,41 +76,35 @@ public enum Suit {
 	public int getSuitOrder() {
 		return sortOrder;
 	}
-	
+
 	/**
 	 * Gets the suit of a card given as string
 	 * 
-	 * @param cardAsString Card as string
+	 * @param cardAsString
+	 *            Card as string
 	 * @return Suit of card
 	 */
 	public static Suit getSuitFromString(String cardAsString) {
-		
+
 		Suit suit = null;
-		
+
 		if (cardAsString.length() == 2) {
 			// parse only, if the string is two characters long
 			if (cardAsString.startsWith("C")) { //$NON-NLS-1$
-				
+
 				suit = CLUBS;
-			}
-			else if (cardAsString.startsWith("S")) { //$NON-NLS-1$
-				
+			} else if (cardAsString.startsWith("S")) { //$NON-NLS-1$
+
 				suit = SPADES;
-			}
-			else if (cardAsString.startsWith("H")) { //$NON-NLS-1$
-				
+			} else if (cardAsString.startsWith("H")) { //$NON-NLS-1$
+
 				suit = HEARTS;
-			}
-			else if (cardAsString.startsWith("D")) { //$NON-NLS-1$
-				
+			} else if (cardAsString.startsWith("D")) { //$NON-NLS-1$
+
 				suit = DIAMONDS;
 			}
 		}
-		
-		return suit;
-	}
 
-	public boolean equals(GameType t) {
-		return this.name().equalsIgnoreCase(t.name());
+		return suit;
 	}
 }
