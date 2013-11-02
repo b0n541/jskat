bfb3a466c555c7343d4d49ba91c928d298be27a4
b0n541 <b0n541@github>
2013-11-02 10:38:44 +0100

    Sascha added to developers

--- jskat-swing-gui/src/main/java/org/jskat/gui/swing/JSkatViewImpl.java
+++ jskat-swing-gui/src/main/java/org/jskat/gui/swing/JSkatViewImpl.java
@@ -499,9 +499,9 @@ public class JSkatViewImpl implements JSkatView {
 								+ "http://sourceforge.net/projects/jskat" //$NON-NLS-1$
 								+ "\n\n" //$NON-NLS-1$
 								+ strings.getString("authors") //$NON-NLS-1$
-								+ ":\nJan Schäfer (jansch@users.sourceforge.net),\nMarkus J. Luzius (jskat@luzius.de),\nDaniel Loreck (daniel.loreck@gmail.com)\n\n" //$NON-NLS-1$
+								+ ":\nJan Schäfer (jansch@users.sourceforge.net),\nMarkus J. Luzius (jskat@luzius.de),\nDaniel Loreck (daniel.loreck@gmail.com),\nSascha Laurien\n\n" //$NON-NLS-1$
 								+ strings.getString("cards") //$NON-NLS-1$
-								+ ": International Skat Server\n\n" //$NON-NLS-1$
+								+ ": International Skat Server, KDE project\n\n" //$NON-NLS-1$
 								+ strings.getString("icons") //$NON-NLS-1$
 								+ ": Gnome Desktop Icons, Tango project, Elementary icons,\n" //$NON-NLS-1$
 								+ "Silvestre Herrera, Alex Roberts and Icojoy\n\n" //$NON-NLS-1$
