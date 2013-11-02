28c745e253995ee869dbfff6722931d3ffe4f77b
b0n541 <b0n541@github>
2013-11-02 11:57:09 +0100

    license headers

--- jskat-base/src/main/java/org/jskat/gui/img/CardFace.java
+++ jskat-base/src/main/java/org/jskat/gui/img/CardFace.java
@@ -1,21 +1,22 @@
 /**
  * JSkat - A skat program written in Java
- * by Jan Schäfer, Markus J. Luzius and Daniel Loreck
+ * by Jan Schäfer, Markus J. Luzius, Daniel Loreck and Sascha Laurien
  *
- * Version 0.13.0-SNAPSHOT
- * Copyright (C) 2013-05-10
+ * Version 0.13.0
+ * Copyright (C) 2013-11-02
  *
- * Licensed under the Apache License, Version 2.0. You may
- * obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jskat.gui.img;
 
