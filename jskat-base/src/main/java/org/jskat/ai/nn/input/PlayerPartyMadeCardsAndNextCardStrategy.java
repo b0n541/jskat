81b52c5e27564211cd74f5bfde5307fa401da034
b0n541 <b0n541@github>
2013-12-08 11:43:44 +0100

    Fixed some issues with maven configuration, added source headers that comply to the standards

--- jskat-base/src/main/java/org/jskat/ai/nn/input/PlayerPartyMadeCardsAndNextCardStrategy.java
+++ jskat-base/src/main/java/org/jskat/ai/nn/input/PlayerPartyMadeCardsAndNextCardStrategy.java
@@ -1,22 +1,17 @@
 /**
- * JSkat - A skat program written in Java
- * by Jan Schäfer, Markus J. Luzius, Daniel Loreck and Sascha Laurien
+ * Copyright (C) 2003 Jan Schäfer (jansch@users.sourceforge.net)
  *
- * Version 0.14.0-SNAPSHOT
- * Copyright (C) 2013-11-02
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jskat.ai.nn.input;
 
