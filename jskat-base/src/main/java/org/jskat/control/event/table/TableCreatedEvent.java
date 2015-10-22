a3fce007772d100fbdba0ceb3d0ff34124e0cb1e
b0n541 <j@nschaefer.net>
2015-10-22 21:03:25 +0200

    added toString() for better readability during debugging

--- jskat-base/src/main/java/org/jskat/control/event/table/TableCreatedEvent.java
+++ jskat-base/src/main/java/org/jskat/control/event/table/TableCreatedEvent.java
@@ -21,18 +21,25 @@ import org.jskat.data.JSkatViewType;
  * This event is created when a skat table is created.
  */
 public class TableCreatedEvent extends AbstractTableEvent {
-	
+
 	public final JSkatViewType tableType;
 
 	/**
 	 * Constructor.
-	 * 
-	 * @param tableType Type to distinguish between local and ISS tables
-	 * @param tableName Table name
+	 *
+	 * @param tableType
+	 *            Type to distinguish between local and ISS tables
+	 * @param tableName
+	 *            Table name
 	 */
 	public TableCreatedEvent(JSkatViewType tableType, String tableName) {
 
 		super(tableName);
 		this.tableType = tableType;
+	}
+
+	@Override
+	public String toString() {
+		return "TableCreatedEvent: type: " + tableType + " name: " + tableName;
 	}
 }
