2c64370b29cb1782f1623f353d3273bf63533c3c
b0n541 <j@nschaefer.net>
2015-12-28 20:13:48 +0100

    removed unused import

--- jskat-base/src/main/java/org/jskat/data/SavePathResolver.java
+++ jskat-base/src/main/java/org/jskat/data/SavePathResolver.java
@@ -15,22 +15,20 @@
  */
 package org.jskat.data;
 
-import java.io.UnsupportedEncodingException;
-
 /**
  * Resolves the default save path for a specific platform
  */
 public interface SavePathResolver {
 	/**
 	 * Resolves the default save path
-	 * 
+	 *
 	 * @return Default save path
 	 */
 	public String getDefaultSavePath();
-	
+
 	/**
 	 * Resolves the current working directory
-	 * 
+	 *
 	 * @return Current working directory
 	 */
 	public String getCurrentWorkingDirectory();
