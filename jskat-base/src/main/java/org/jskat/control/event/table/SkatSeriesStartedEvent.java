4932c753dac63271de565d944751d392d1126a1c
b0n541 <j@nschaefer.net>
2017-09-15 13:58:24 +0200

    Fix logging configuration

--- jskat-base/src/main/java/org/jskat/control/event/table/SkatSeriesStartedEvent.java
+++ jskat-base/src/main/java/org/jskat/control/event/table/SkatSeriesStartedEvent.java
@@ -17,7 +17,12 @@ package org.jskat.control.event.table;
 
 public class SkatSeriesStartedEvent extends AbstractTableEvent {
 
-	public SkatSeriesStartedEvent(String tableName) {
+	public SkatSeriesStartedEvent(final String tableName) {
 		super(tableName);
+	}
+
+	@Override
+	public String toString() {
+		return "SkatSeriesStartedEvent: tableName: " + tableName;
 	}
 }
