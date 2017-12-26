ddf04dc7ec7cbd5f0971edd3e133994d73f00d76
b0n541 <j@nschaefer.net>
2017-12-26 18:02:01 +0100

    Rename test class

--- jskat-base/src/test/java/org/jskat/ai/newalgorithm/AlgorithmOpponentGrandTest.java
+++ jskat-base/src/test/java/org/jskat/ai/newalgorithm/AlgorithmOpponentGrandTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2017 Jan Schäfer (jansch@users.sourceforge.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jskat.ai.newalgorithm;
 
 import static org.hamcrest.Matchers.is;
@@ -26,7 +41,7 @@ import org.junit.Test;
 import com.google.common.collect.Lists;
 import com.google.common.eventbus.EventBus;
 
-public class Issue57Bug extends AbstractJSkatTest {
+public class AlgorithmOpponentGrandTest extends AbstractJSkatTest {
 
 	private static final String TABLE_NAME = "Issue 57 Bug";
 
@@ -37,7 +52,7 @@ public class Issue57Bug extends AbstractJSkatTest {
 	}
 
 	@Test
-	public void testIssue57Bug() {
+	public void testOpponentPlayerFollowsJackInGrandGame() {
 		final UnitTestPlayer deterministicPlayer = new UnitTestPlayer();
 		deterministicPlayer.setCardsToPlay(Lists.newArrayList(Card.SJ));
 		final AlgorithmAI faultyPlayer = new AlgorithmAI();
