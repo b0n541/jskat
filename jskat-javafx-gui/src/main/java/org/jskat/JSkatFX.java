4f1fc357ab4b20d62e5c6ed89dc2963f197c9dab
b0n541 <j@nschaefer.net>
2019-09-13 22:30:18 +0200

    Centralize styling of all JavaFX parts

--- jskat-javafx-gui/src/main/java/org/jskat/JSkatFX.java
+++ jskat-javafx-gui/src/main/java/org/jskat/JSkatFX.java
@@ -1,38 +1,20 @@
 /**
  * This file is part of JSkat.
- *
+ * <p>
  * JSkat is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * JSkat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with JSkat.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jskat;
-
-import java.awt.Point;
-
-import javax.swing.SwingUtilities;
-
-import org.jskat.control.JSkatEventBus;
-import org.jskat.control.JSkatMaster;
-import org.jskat.control.command.general.ShowWelcomeInformationCommand;
-import org.jskat.data.DesktopSavePathResolver;
-import org.jskat.data.JSkatOptions;
-import org.jskat.data.JSkatOptions.Option;
-import org.jskat.gui.img.JSkatGraphicRepository;
-import org.jskat.gui.javafx.JSkatMainWindow;
-import org.jskat.gui.javafx.JSkatMenuFactory;
-import org.jskat.gui.swing.JSkatViewImpl;
-import org.jskat.gui.swing.LookAndFeelSetter;
-import org.jskat.util.JSkatResourceBundle;
-import org.jskat.util.version.VersionChecker;
 
 import javafx.animation.FadeTransition;
 import javafx.application.Application;
@@ -54,154 +36,168 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.util.Duration;
+import org.jskat.control.JSkatEventBus;
+import org.jskat.control.JSkatMaster;
+import org.jskat.control.command.general.ShowWelcomeInformationCommand;
+import org.jskat.data.DesktopSavePathResolver;
+import org.jskat.data.JSkatOptions;
+import org.jskat.data.JSkatOptions.Option;
+import org.jskat.gui.img.JSkatGraphicRepository;
+import org.jskat.gui.javafx.JSkatMainWindow;
+import org.jskat.gui.javafx.JSkatMenuFactory;
+import org.jskat.gui.swing.JSkatViewImpl;
+import org.jskat.gui.swing.LookAndFeelSetter;
+import org.jskat.util.JSkatResourceBundle;
+import org.jskat.util.version.VersionChecker;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class JSkatFX extends Application {
 
-	private static final String VERSION = "0.17.0";
+    private static final String VERSION = "0.17.0";
 
-	private static final int SPLASH_WIDTH = 500;
-	private static final int SPLASH_HEIGHT = 300;
+    private static final int SPLASH_WIDTH = 500;
+    private static final int SPLASH_HEIGHT = 300;
 
-	private Pane splashScreenLayout;
-	private ProgressBar splashScreenProgressBar;
-	private Label splashScreenProgressText;
+    private Pane splashScreenLayout;
+    private ProgressBar splashScreenProgressBar;
+    private Label splashScreenProgressText;
 
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 
-		JSkatOptions.instance(new DesktopSavePathResolver());
+        JSkatOptions.instance(new DesktopSavePathResolver());
 
-		launch(args);
-	}
+        launch(args);
+    }
 
-	@Override
-	public void init() {
-		final ImageView splashScreenImage = new ImageView(
-				new Image(JSkatFX.class.getResource("/org/jskat/gui/img/gui/splash.png").toExternalForm()));
-		splashScreenProgressBar = new ProgressBar();
-		splashScreenProgressBar.setPrefWidth(SPLASH_WIDTH);
-		splashScreenProgressText = new Label("Loading JSkat...");
-		splashScreenProgressText.setAlignment(Pos.CENTER);
-		splashScreenLayout = new VBox();
-		splashScreenLayout.getChildren().addAll(splashScreenImage, splashScreenProgressBar, splashScreenProgressText);
-		splashScreenLayout.setStyle(
-				"-fx-padding: 5; " + "-fx-background-color: #e2d9ca; " + "-fx-border-width:2; " + "-fx-border-color: "
-						+ "linear-gradient(" + "to bottom, " + "chocolate, " + "derive(chocolate, 50%)" + ");");
-		splashScreenLayout.setEffect(new DropShadow());
-	}
+    @Override
+    public void init() {
+        final ImageView splashScreenImage = new ImageView(
+                new Image(JSkatFX.class.getResource("/org/jskat/gui/img/gui/splash.png").toExternalForm()));
+        splashScreenProgressBar = new ProgressBar();
+        splashScreenProgressBar.setPrefWidth(SPLASH_WIDTH);
+        splashScreenProgressText = new Label("Loading JSkat...");
+        splashScreenProgressText.setAlignment(Pos.CENTER);
+        splashScreenLayout = new VBox();
+        splashScreenLayout.getChildren().addAll(splashScreenImage, splashScreenProgressBar, splashScreenProgressText);
+        splashScreenLayout.setStyle("-fx-padding: 5; -fx-spacing: 5; -fx-border-width:2;");
+        splashScreenLayout.setEffect(new DropShadow());
+    }
 
-	@Override
-	public void start(final Stage primaryStage) {
+    @Override
+    public void start(final Stage primaryStage) {
 
-		final Point2D savedScreenPosition = getSavedScreenPosition();
-		final Screen targetScreen = getTargetScreen(savedScreenPosition);
+        final Point2D savedScreenPosition = getSavedScreenPosition();
+        final Screen targetScreen = getTargetScreen(savedScreenPosition);
 
-		final Task<InitializedGuiElements> startupTasks = new Task<InitializedGuiElements>() {
-			@Override
-			protected InitializedGuiElements call() throws Exception {
+        final Task<InitializedGuiElements> startupTasks = new Task<InitializedGuiElements>() {
+            @Override
+            protected InitializedGuiElements call() throws Exception {
 
-				updateMessage(JSkatResourceBundle.INSTANCE.getString("splash_init_application"));
+                updateMessage(JSkatResourceBundle.INSTANCE.getString("splash_init_application"));
 
-				SwingUtilities.invokeLater(() -> LookAndFeelSetter.setLookAndFeel(targetScreen));
+                SwingUtilities.invokeLater(() -> LookAndFeelSetter.setLookAndFeel(targetScreen));
 
-				updateProgress(1, 3);
-				updateMessage(JSkatResourceBundle.INSTANCE.getString("splash_load_card_sets"));
+                updateProgress(1, 3);
+                updateMessage(JSkatResourceBundle.INSTANCE.getString("splash_load_card_sets"));
 
-				JSkatGraphicRepository.INSTANCE.toString();
+                JSkatGraphicRepository.INSTANCE.toString();
 
-				updateProgress(2, 3);
-				updateMessage(JSkatResourceBundle.INSTANCE.getString("splash_look_for_ai_players"));
+                updateProgress(2, 3);
+                updateMessage(JSkatResourceBundle.INSTANCE.getString("splash_look_for_ai_players"));
 
-				final MenuBar menu = JSkatMenuFactory.build();
-				final JSkatViewImpl jskatView = new JSkatViewImpl(targetScreen, menu, VERSION);
-				JSkatMaster.INSTANCE.setView(jskatView);
+                final MenuBar menu = JSkatMenuFactory.build();
+                final JSkatViewImpl jskatView = new JSkatViewImpl(targetScreen, menu, VERSION);
+                JSkatMaster.INSTANCE.setView(jskatView);
 
-				return new InitializedGuiElements(menu, jskatView);
-			}
-		};
+                return new InitializedGuiElements(menu, jskatView);
+            }
+        };
 
-		showSplashScreen(targetScreen, primaryStage, startupTasks,
-				() -> showJSkatMainWindow(targetScreen, savedScreenPosition, startupTasks.valueProperty().get().menu,
-						startupTasks.valueProperty().get().jskatView));
+        showSplashScreen(targetScreen, primaryStage, startupTasks,
+                () -> showJSkatMainWindow(targetScreen, savedScreenPosition, startupTasks.valueProperty().get().menu,
+                        startupTasks.valueProperty().get().jskatView));
 
-		new Thread(startupTasks).start();
-	}
+        new Thread(startupTasks).start();
+    }
 
-	private static void showJSkatMainWindow(final Screen targetScreen, final Point2D screenPosition, final MenuBar menu,
-			final JSkatViewImpl jskatView) {
+    private static void showJSkatMainWindow(final Screen targetScreen, final Point2D screenPosition, final MenuBar menu,
+                                            final JSkatViewImpl jskatView) {
 
-		final JSkatMainWindow jskatMainWindow = new JSkatMainWindow(VERSION, menu, jskatView, targetScreen,
-				screenPosition);
+        final JSkatMainWindow jskatMainWindow = new JSkatMainWindow(VERSION, menu, jskatView, targetScreen,
+                screenPosition);
 
-		jskatMainWindow.show();
+        jskatMainWindow.show();
 
-		if (JSkatOptions.instance().getBoolean(Option.SHOW_TIPS_AT_START_UP)) {
-			JSkatEventBus.INSTANCE.post(new ShowWelcomeInformationCommand());
-		}
+        if (JSkatOptions.instance().getBoolean(Option.SHOW_TIPS_AT_START_UP)) {
+            JSkatEventBus.INSTANCE.post(new ShowWelcomeInformationCommand());
+        }
 
-		if (JSkatOptions.instance().getBoolean(Option.CHECK_FOR_NEW_VERSION_AT_START_UP)) {
-			JSkatMaster.INSTANCE.checkJSkatVersion(VERSION, VersionChecker.getLatestVersion());
-		}
-	}
+        if (JSkatOptions.instance().getBoolean(Option.CHECK_FOR_NEW_VERSION_AT_START_UP)) {
+            JSkatMaster.INSTANCE.checkJSkatVersion(VERSION, VersionChecker.getLatestVersion());
+        }
+    }
 
-	private void showSplashScreen(final Screen targetScreen, final Stage splashStage, final Task<?> startupTask,
-			final InitializationCompleteHandler initializationCompleteHandler) {
+    private void showSplashScreen(final Screen targetScreen, final Stage splashStage, final Task<?> startupTask,
+                                  final InitializationCompleteHandler initializationCompleteHandler) {
 
-		splashScreenProgressText.textProperty().bind(startupTask.messageProperty());
-		splashScreenProgressBar.progressProperty().bind(startupTask.progressProperty());
+        splashScreenProgressText.textProperty().bind(startupTask.messageProperty());
+        splashScreenProgressBar.progressProperty().bind(startupTask.progressProperty());
 
-		startupTask.stateProperty().addListener((progressValue, oldState, newState) -> {
-			if (Worker.State.SUCCEEDED.equals(newState)) {
-				splashScreenProgressBar.progressProperty().unbind();
-				splashScreenProgressBar.setProgress(1);
-				splashStage.toFront();
-				final FadeTransition fadeSplashScreen = new FadeTransition(Duration.seconds(1), splashScreenLayout);
-				fadeSplashScreen.setFromValue(1.0);
-				fadeSplashScreen.setToValue(0.0);
-				fadeSplashScreen.setOnFinished(actionEvent -> splashStage.hide());
-				fadeSplashScreen.play();
+        startupTask.stateProperty().addListener((progressValue, oldState, newState) -> {
+            if (Worker.State.SUCCEEDED.equals(newState)) {
+                splashScreenProgressBar.progressProperty().unbind();
+                splashScreenProgressBar.setProgress(1);
+                splashStage.toFront();
+                final FadeTransition fadeSplashScreen = new FadeTransition(Duration.seconds(1), splashScreenLayout);
+                fadeSplashScreen.setFromValue(1.0);
+                fadeSplashScreen.setToValue(0.0);
+                fadeSplashScreen.setOnFinished(actionEvent -> splashStage.hide());
+                fadeSplashScreen.play();
 
-				initializationCompleteHandler.complete();
-			}
-		});
+                initializationCompleteHandler.complete();
+            }
+        });
 
-		final Scene splashScene = new Scene(splashScreenLayout);
-		splashStage.initStyle(StageStyle.UNDECORATED);
-		splashStage.setScene(splashScene);
-		final Rectangle2D bounds = targetScreen.getBounds();
-		splashStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
-		splashStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        final Scene splashScene = new Scene(splashScreenLayout);
+        splashStage.initStyle(StageStyle.UNDECORATED);
+        splashStage.setScene(splashScene);
+        final Rectangle2D bounds = targetScreen.getBounds();
+        splashStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        splashStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
 
-		splashStage.show();
-	}
+        splashStage.show();
+    }
 
-	private static Screen getTargetScreen(final Point2D savedScreenPosition) {
+    private static Screen getTargetScreen(final Point2D savedScreenPosition) {
 
-		for (final Screen screen : Screen.getScreens()) {
-			if (screen.getVisualBounds().contains(savedScreenPosition)) {
-				return screen;
-			}
-		}
+        for (final Screen screen : Screen.getScreens()) {
+            if (screen.getVisualBounds().contains(savedScreenPosition)) {
+                return screen;
+            }
+        }
 
-		return Screen.getPrimary();
-	}
+        return Screen.getPrimary();
+    }
 
-	private static Point2D getSavedScreenPosition() {
+    private static Point2D getSavedScreenPosition() {
 
-		final Point mainFramePosition = JSkatOptions.instance().getMainFramePosition();
-		return new Point2D(mainFramePosition.getX(), mainFramePosition.getY());
-	}
+        final Point mainFramePosition = JSkatOptions.instance().getMainFramePosition();
+        return new Point2D(mainFramePosition.getX(), mainFramePosition.getY());
+    }
 
-	private interface InitializationCompleteHandler {
-		public void complete();
-	}
+    private interface InitializationCompleteHandler {
+        public void complete();
+    }
 
-	private class InitializedGuiElements {
-		final MenuBar menu;
-		final JSkatViewImpl jskatView;
+    private class InitializedGuiElements {
+        final MenuBar menu;
+        final JSkatViewImpl jskatView;
 
-		public InitializedGuiElements(final MenuBar menu, final JSkatViewImpl jskatView) {
-			this.menu = menu;
-			this.jskatView = jskatView;
-		}
-	}
+        public InitializedGuiElements(final MenuBar menu, final JSkatViewImpl jskatView) {
+            this.menu = menu;
+            this.jskatView = jskatView;
+        }
+    }
 }
--- jskat-javafx-gui/src/main/java/org/jskat/gui/javafx/JSkatMainWindow.java
+++ jskat-javafx-gui/src/main/java/org/jskat/gui/javafx/JSkatMainWindow.java
@@ -1,35 +1,22 @@
 /**
  * This file is part of JSkat.
- *
+ * <p>
  * JSkat is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * JSkat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with JSkat.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jskat.gui.javafx;
 
-import java.io.IOException;
-
-import org.jskat.JSkatFX;
-import org.jskat.control.JSkatEventBus;
-import org.jskat.control.JSkatMaster;
-import org.jskat.control.command.general.ShowWelcomeInformationCommand;
-import org.jskat.control.event.general.NewJSkatVersionAvailableEvent;
-import org.jskat.data.JSkatOptions;
-import org.jskat.gui.img.JSkatGraphicRepository;
-import org.jskat.gui.swing.JSkatViewImpl;
-import org.jskat.util.JSkatResourceBundle;
-
 import com.google.common.eventbus.Subscribe;
-
 import javafx.embed.swing.SwingNode;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -43,112 +30,119 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.stage.WindowEvent;
+import javafx.stage.*;
+import org.jskat.JSkatFX;
+import org.jskat.control.JSkatEventBus;
+import org.jskat.control.JSkatMaster;
+import org.jskat.control.command.general.ShowWelcomeInformationCommand;
+import org.jskat.control.event.general.NewJSkatVersionAvailableEvent;
+import org.jskat.data.JSkatOptions;
+import org.jskat.gui.img.JSkatGraphicRepository;
+import org.jskat.gui.swing.JSkatViewImpl;
+import org.jskat.util.JSkatResourceBundle;
+
+import java.io.IOException;
 
 /**
  * Main window for JSkat.
  */
 public class JSkatMainWindow extends Stage {
 
-	private static JSkatResourceBundle STRINGS = JSkatResourceBundle.INSTANCE;
+    private static final JSkatResourceBundle STRINGS = JSkatResourceBundle.INSTANCE;
 
-	public JSkatMainWindow(String version, MenuBar menu, JSkatViewImpl jskatView, Screen targetScreen,
-			Point2D screenPosition) {
+    public JSkatMainWindow(final String version, final MenuBar menu, final JSkatViewImpl jskatView, final Screen targetScreen,
+                           final Point2D screenPosition) {
 
-		super(StageStyle.DECORATED);
+        super(StageStyle.DECORATED);
 
-		JSkatEventBus.INSTANCE.register(this);
+        JSkatEventBus.INSTANCE.register(this);
 
-		setTitle("JSkat " + version);
+        setTitle("JSkat " + version);
 
-		getIcons().add(JSkatGraphicRepository.INSTANCE.getJSkatLogoImageFX());
+        getIcons().add(JSkatGraphicRepository.INSTANCE.getJSkatLogoImageFX());
 
-		SwingNode swingNode = new SwingNode();
-		swingNode.setContent(jskatView.mainPanel);
+        final SwingNode swingNode = new SwingNode();
+        swingNode.setContent(jskatView.mainPanel);
 
-		VBox pane = new VBox();
-		pane.getChildren().addAll(menu, swingNode);
-		VBox.setVgrow(swingNode, Priority.ALWAYS);
+        final VBox pane = new VBox();
+        pane.getChildren().addAll(menu, swingNode);
+        VBox.setVgrow(swingNode, Priority.ALWAYS);
 
-		Dimension2D dimension = getMainWindowDimension(targetScreen);
-		Scene scene = new Scene(pane, dimension.getWidth(), dimension.getHeight());
-		scene.widthProperty().addListener(
-				(observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameWidth(newValue.intValue()));
-		scene.heightProperty().addListener(
-				(observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameHeight(newValue.intValue()));
+        final Dimension2D dimension = getMainWindowDimension(targetScreen);
+        final Scene scene = new Scene(pane, dimension.getWidth(), dimension.getHeight());
+        scene.widthProperty().addListener(
+                (observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameWidth(newValue.intValue()));
+        scene.heightProperty().addListener(
+                (observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameHeight(newValue.intValue()));
 
-		setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("/org/jskat/gui/javafx/jskat.css").toExternalForm());
 
-		xProperty().addListener(
-				(observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameXPosition(newValue.intValue()));
-		yProperty().addListener(
-				(observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameYPosition(newValue.intValue()));
+        setScene(scene);
 
-		setOnCloseRequest(new EventHandler<WindowEvent>() {
-			@Override
-			public void handle(WindowEvent we) {
-				JSkatMaster.INSTANCE.exitJSkat();
-			}
-		});
+        xProperty().addListener(
+                (observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameXPosition(newValue.intValue()));
+        yProperty().addListener(
+                (observable, oldValue, newValue) -> JSkatOptions.instance().setMainFrameYPosition(newValue.intValue()));
 
-		placeMainWindow(targetScreen, this, screenPosition);
-	}
+        setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(final WindowEvent we) {
+                JSkatMaster.INSTANCE.exitJSkat();
+            }
+        });
 
-	private static Dimension2D getMainWindowDimension(Screen targetScreen) {
-		double width = JSkatOptions.instance().getMainFrameSize().getWidth();
-		double height = JSkatOptions.instance().getMainFrameSize().getHeight();
+        placeMainWindow(targetScreen, this, screenPosition);
+    }
 
-		// on first startup the default values for width and height are
-		// Integer.MIN_VALUE
-		return new Dimension2D(width > 0 ? width : targetScreen.getBounds().getWidth() * 2 / 3,
-				height > 0 ? height : targetScreen.getBounds().getHeight() * 2 / 3);
-	}
+    private static Dimension2D getMainWindowDimension(final Screen targetScreen) {
+        final double width = JSkatOptions.instance().getMainFrameSize().getWidth();
+        final double height = JSkatOptions.instance().getMainFrameSize().getHeight();
 
-	private static void placeMainWindow(Screen screen, Stage mainWindow, Point2D position) {
+        // on first startup the default values for width and height are
+        // Integer.MIN_VALUE
+        return new Dimension2D(width > 0 ? width : targetScreen.getBounds().getWidth() * 2 / 3,
+                height > 0 ? height : targetScreen.getBounds().getHeight() * 2 / 3);
+    }
 
-		if (screen.getVisualBounds().contains(position)) {
-			mainWindow.setX(position.getX());
-			mainWindow.setY(position.getY());
-		} else {
-			mainWindow.centerOnScreen();
-		}
-	}
+    private static void placeMainWindow(final Screen screen, final Stage mainWindow, final Point2D position) {
 
-	@Subscribe
-	public void showInfoMessageOn(NewJSkatVersionAvailableEvent event) {
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.initOwner(this);
-		alert.setTitle(STRINGS.getString("new_version_title"));
-		alert.setHeaderText(STRINGS.getString("new_version_header", event.newVersion));
-		alert.setContentText(STRINGS.getString("new_version_message", event.newVersion));
+        if (screen.getVisualBounds().contains(position)) {
+            mainWindow.setX(position.getX());
+            mainWindow.setY(position.getY());
+        } else {
+            mainWindow.centerOnScreen();
+        }
+    }
 
-		// this is a workaround for a bug under Linux that cuts long texts
-		alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label)
-				.forEach(node -> ((Label) node).setMinHeight(Region.USE_PREF_SIZE));
+    @Subscribe
+    public void showInfoMessageOn(final NewJSkatVersionAvailableEvent event) {
+        final Alert alert = new Alert(AlertType.INFORMATION);
+        alert.initOwner(this);
+        alert.setTitle(STRINGS.getString("new_version_title"));
+        alert.setHeaderText(STRINGS.getString("new_version_header", event.newVersion));
+        alert.setContentText(STRINGS.getString("new_version_message", event.newVersion));
 
-		alert.getDialogPane().setStyle("-fx-background-color: #e2d9ca;");
+        // this is a workaround for a bug under Linux that cuts long texts
+        alert.getDialogPane().getChildren().stream().filter(node -> node instanceof Label)
+                .forEach(node -> ((Label) node).setMinHeight(Region.USE_PREF_SIZE));
 
-		alert.showAndWait();
-	}
+        alert.showAndWait();
+    }
 
-	@Subscribe
-	public void showWelcomeDialogOn(ShowWelcomeInformationCommand command) throws IOException {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(
-				JSkatFX.class.getResource("/org/jskat/gui/javafx/dialog/firststeps/view/FirstStepsDialog.fxml"));
-		loader.setResources(JSkatResourceBundle.INSTANCE.getStringResources());
-		VBox rootLayout;
-		rootLayout = (VBox) loader.load();
-		Stage stage = new Stage();
-		stage.setTitle(JSkatResourceBundle.INSTANCE.getString("show_tips"));
-		Scene scene = new Scene(rootLayout);
-		stage.setScene(scene);
-		stage.initModality(Modality.APPLICATION_MODAL);
-		stage.initOwner(this);
-		stage.show();
-	}
+    @Subscribe
+    public void showWelcomeDialogOn(final ShowWelcomeInformationCommand command) throws IOException {
+        final FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(
+                JSkatFX.class.getResource("/org/jskat/gui/javafx/dialog/firststeps/view/FirstStepsDialog.fxml"));
+        loader.setResources(JSkatResourceBundle.INSTANCE.getStringResources());
+        final VBox rootLayout;
+        rootLayout = (VBox) loader.load();
+        final Stage stage = new Stage();
+        stage.setTitle(JSkatResourceBundle.INSTANCE.getString("show_tips"));
+        final Scene scene = new Scene(rootLayout);
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.initOwner(this);
+        stage.show();
+    }
 }
