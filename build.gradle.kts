86ee7496c7c2aa66f58cc7ac1a98f825c388c16a
b0n541 <j@nschaefer.net>
2020-09-07 19:54:23 +0200

    Revert to JDK 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,8 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_14
-        targetCompatibility = JavaVersion.VERSION_14
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
 
         withSourcesJar()
         withJavadocJar()
