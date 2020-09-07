fdfd16ad0f89832b5e4200cddb622ac8298118bb
b0n541 <j@nschaefer.net>
2020-09-07 20:29:31 +0200

    Add FAT JAR task

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,20 @@ tasks {
             )
         }
     }
+
+    register("fatJar", Jar::class.java) {
+        archiveClassifier.set("all")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes("Main-Class" to "org.jskat.Launcher")
+        }
+        from(configurations.runtimeClasspath.get()
+            .onEach { println("add from dependencies: ${it.name}") }
+            .map { if (it.isDirectory) it else zipTree(it) })
+        val sourcesMain = sourceSets.main.get()
+        sourcesMain.allSource.forEach { println("add from sources: ${it.name}") }
+        from(sourcesMain.output)
+    }
 }
 
 application {
