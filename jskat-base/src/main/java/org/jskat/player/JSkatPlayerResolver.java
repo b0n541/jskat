0ebb115b54db6c7a0c05d7d0f517d9e3bf20f89e
b0n541 <j@nschaefer.net>
2021-01-30 20:50:42 +0100

    Fix scanning of AI player classes to prevent exception when running with fat JAR

--- jskat-base/src/main/java/org/jskat/player/JSkatPlayerResolver.java
+++ jskat-base/src/main/java/org/jskat/player/JSkatPlayerResolver.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2020 Jan Schäfer (jansch@users.sourceforge.net)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,16 +15,14 @@
  */
 package org.jskat.player;
 
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.Set;
-
-import org.reflections.Reflections;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Resolves all implementations of {@link JSkatPlayer} that inherit from
@@ -32,70 +30,68 @@ import org.slf4j.LoggerFactory;
  */
 public class JSkatPlayerResolver {
 
-	private final static Logger LOG = LoggerFactory.getLogger(JSkatPlayerResolver.class);
+    private final static Logger LOG = LoggerFactory.getLogger(JSkatPlayerResolver.class);
 
-	public static final String HUMAN_PLAYER_CLASS = "org.jskat.gui.human.SwingHumanPlayer";
+    public static final String HUMAN_PLAYER_CLASS = "org.jskat.gui.human.SwingHumanPlayer";
 
-	public static final Set<String> EXCLUDED_PLAYER_CLASSES;
+    public static final Set<String> EXCLUDED_PLAYER_CLASSES;
 
-	public static final Set<String> UNIT_TEST_PLAYER_CLASSES;
+    public static final Set<String> UNIT_TEST_PLAYER_CLASSES;
 
-	static {
-		EXCLUDED_PLAYER_CLASSES = new HashSet<String>();
-		EXCLUDED_PLAYER_CLASSES.add("org.jskat.ai.mjl.AIPlayerMJL");
-		EXCLUDED_PLAYER_CLASSES.add("org.jskat.ai.algorithmic.AlgorithmicAIPlayer");
+    static {
+        EXCLUDED_PLAYER_CLASSES = new HashSet<>();
+        EXCLUDED_PLAYER_CLASSES.add("org.jskat.ai.mjl.AIPlayerMJL");
+        EXCLUDED_PLAYER_CLASSES.add("org.jskat.ai.algorithmic.AlgorithmicAIPlayer");
 
-		UNIT_TEST_PLAYER_CLASSES = new HashSet<String>();
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.UnitTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.RamschTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.NoBiddingTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.ContraCallingTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.ContraReCallingTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.ExceptionTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.PlayNonPossessingCardTestPlayer");
-		UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.PlayNotAllowedCardTestPlayer");
-	}
+        UNIT_TEST_PLAYER_CLASSES = new HashSet<>();
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.UnitTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.RamschTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.NoBiddingTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.ContraCallingTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.ContraReCallingTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.ExceptionTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.PlayNonPossessingCardTestPlayer");
+        UNIT_TEST_PLAYER_CLASSES.add("org.jskat.ai.test.PlayNotAllowedCardTestPlayer");
+    }
 
-	/**
-	 * Gets all class names including package names of AI player implementations
-	 *
-	 * @return A set of all player implementation classes
-	 */
-	public static Set<String> getAllAIPlayerImplementations() {
+    /**
+     * Gets all class names including package names of AI player implementations
+     *
+     * @return A set of all player implementation classes
+     */
+    public static Set<String> getAllAIPlayerImplementations() {
 
-		final Set<String> result = getAllImplementations();
+        Set<String> result = getAllImplementations();
 
-		result.removeAll(EXCLUDED_PLAYER_CLASSES);
-		result.removeAll(UNIT_TEST_PLAYER_CLASSES);
+        result.removeAll(EXCLUDED_PLAYER_CLASSES);
+        result.removeAll(UNIT_TEST_PLAYER_CLASSES);
 
-		LOG.info("Found {} implementations of AbstractJSkatPlayer.", result.size());
+        LOG.info("Found {} implementations of AbstractJSkatPlayer.", result.size());
 
-		return result;
-	}
+        return result;
+    }
 
-	private static Set<String> getAllImplementations() {
-		final Set<String> result = new HashSet<String>();
-		final Reflections reflections = new Reflections(new ConfigurationBuilder()
-				.setUrls(ClasspathHelper.forPackage("org.jskat.ai"))
-				.filterInputsBy(new FilterBuilder().include(".*\\.class")));
+    private static Set<String> getAllImplementations() {
+        Set<String> result = new HashSet<>();
+        Reflections reflections = new Reflections("org.jskat.ai", new SubTypesScanner());
 
-		final Set<Class<? extends AbstractJSkatPlayer>> subTypes = reflections.getSubTypesOf(AbstractJSkatPlayer.class);
-		for (final Class<? extends AbstractJSkatPlayer> jskatPlayer : subTypes) {
-			if (isNotAbstract(jskatPlayer) && isNotHumanPlayer(jskatPlayer)) {
-				result.add(jskatPlayer.getName());
-			}
-		}
+        Set<Class<? extends AbstractJSkatPlayer>> subTypes = reflections.getSubTypesOf(AbstractJSkatPlayer.class);
+        for (Class<? extends AbstractJSkatPlayer> jskatPlayer : subTypes) {
+            if (isNotAbstract(jskatPlayer) && isNotHumanPlayer(jskatPlayer)) {
+                result.add(jskatPlayer.getName());
+            }
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private static boolean isNotHumanPlayer(
-			final Class<? extends AbstractJSkatPlayer> jskatPlayer) {
-		return !HUMAN_PLAYER_CLASS.equals(jskatPlayer.getName());
-	}
+    private static boolean isNotHumanPlayer(
+			Class<? extends AbstractJSkatPlayer> jskatPlayer) {
+        return !HUMAN_PLAYER_CLASS.equals(jskatPlayer.getName());
+    }
 
-	private static boolean isNotAbstract(
-			final Class<? extends AbstractJSkatPlayer> jskatPlayer) {
-		return !Modifier.isAbstract(jskatPlayer.getModifiers());
-	}
+    private static boolean isNotAbstract(
+			Class<? extends AbstractJSkatPlayer> jskatPlayer) {
+        return !Modifier.isAbstract(jskatPlayer.getModifiers());
+    }
 }
