37b52fca81a6cbb25c0f43273f55b4e8c3851d4c
b0n541 <j@nschaefer.net>
2021-04-05 18:14:25 +0200

    Add module information

---
+++ jskat-swing-gui/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module jskat.swing {
+    requires java.desktop;
+
+    requires javafx.controls;
+
+    requires org.slf4j;
+    requires com.google.common;
+
+    requires jskat.base;
+
+    exports org.jskat.gui.img;
+    exports org.jskat.gui.swing;
+}
