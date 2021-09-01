c8edf42c0410d445b4dcedc5f6e33aa845de2edd
b0n541 <j@nschaefer.net>
2021-09-01 22:44:35 +0200

    Convert into network input information for Deeplearning4J

--- jskat-base/src/main/java/org/jskat/control/iss/IssGameExtractor.java
+++ jskat-base/src/main/java/org/jskat/control/iss/IssGameExtractor.java
@@ -15,12 +15,16 @@
  */
 package org.jskat.control.iss;
 
+import org.jskat.data.SkatGameData;
+import org.jskat.util.Card;
+import org.jskat.util.CardList;
 import org.jskat.util.Player;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,35 +34,49 @@ import java.util.stream.Stream;
  */
 public class IssGameExtractor {
 
-    private static final Logger log = LoggerFactory.getLogger(IssGameExtractor.class);
-
-    private static String fileName;
+    private final String fileName;
 
     public static void main(final String args[]) throws Exception {
-
-        final IssGameExtractor gameExtractor = new IssGameExtractor();
-        gameExtractor.setFilePath("/home/jan/Projects/jskat/iss/iss-games-04-2021.sgf"); //$NON-NLS-1$
-
-        filterGameDatabase();
+        final IssGameExtractor gameExtractor = new IssGameExtractor("/home/jan/Projects/jskat/iss/iss-games-04-2021.sgf");
+        gameExtractor.filterGameDatabase(KERMIT_FOREHAND_GAMES);
     }
 
-    private static void filterGameDatabase() throws Exception {
+    public IssGameExtractor(final String fileName) {
+        this.fileName = fileName;
+    }
+
+    private static final Predicate<SkatGameData> KERMIT_FOREHAND_GAMES =
+            it -> it.getDeclarer() == Player.FOREHAND
+                    && it.getPlayerName(Player.FOREHAND).startsWith("kermit")
+                    && it.isGameWon();
+
+    private static final Function<SkatGameData, String> NETWORK_INPUTS = it ->
+            it.getDealtCards().get(Player.FOREHAND) + ": "
+                    + asNetworkInputs(it.getDealtCards().get(Player.FOREHAND))
+                    + it.getMaxPlayerBid(Player.FOREHAND) + ","
+                    + it.getAnnoucement().getGameType() + ","
+                    + (it.getAnnoucement().isHand() ? "1" : "0") + ","
+                    + (it.getAnnoucement().isOuvert() ? "1" : "0") + ","
+                    + (it.getAnnoucement().isSchneider() ? "1" : "0") + ","
+                    + (it.getAnnoucement().isSchwarz() ? "1" : "0");
+
+    private void filterGameDatabase(final Predicate<SkatGameData> predicate) throws Exception {
 
         try (final Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            final var kermitForehandGames = stream.map(MessageParser::parseGameSummary)
-                    .filter(it -> it.getDeclarer() == Player.FOREHAND && it.getPlayerName(Player.FOREHAND).startsWith("kermit"))
-                    .filter(it -> it.isGameWon())
-                    .limit(100)
+            final var filteredGames = stream.map(MessageParser::parseGameSummary)
+                    .filter(predicate)
+                    .map(NETWORK_INPUTS)
+                    .peek(System.out::println)
+                    .limit(10000)
                     .collect(Collectors.toList());
 
-            kermitForehandGames.forEach(it -> System.out.println(" "
-                    + it.getDealtCards().get(Player.FOREHAND) + " "
-                    + it.getMaxPlayerBid(Player.FOREHAND) + " "
-                    + it.getAnnoucement().getGameType()));
+            // TODO save into file
         }
     }
 
-    public static void setFilePath(final String newFileName) {
-        fileName = newFileName;
+    private static String asNetworkInputs(final CardList cards) {
+        final StringBuffer result = new StringBuffer();
+        Arrays.stream(Card.values()).forEach(it -> result.append(cards.contains(it) ? "1," : "0,"));
+        return result.toString();
     }
 }
