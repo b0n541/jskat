5af7e7f274b392b3005e591df4540ee6a591b724
b0n541 <j@nschaefer.net>
2022-12-30 17:00:58 +0100

    #140 Fix events for bidding

--- jskat-javafx-gui/src/main/java/org/jskat/Launcher.java
+++ jskat-javafx-gui/src/main/java/org/jskat/Launcher.java
@@ -4,7 +4,6 @@ package org.jskat;
  * This class is needed to start JSkat from a fat jar.
  */
 public class Launcher {
-
     public static void main(final String[] args) {
         JSkatFX.main(args);
     }
