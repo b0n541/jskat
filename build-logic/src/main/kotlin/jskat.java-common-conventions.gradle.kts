ea098f447dddc74629b5b57b7efc395e3d0f51a7
b0n541 <j@nschaefer.net>
2023-04-26 21:52:49 +0200

    Define dependencies explicitly to help dependabot

--- build-logic/src/main/kotlin/jskat.java-common-conventions.gradle.kts
+++ build-logic/src/main/kotlin/jskat.java-common-conventions.gradle.kts
@@ -8,16 +8,14 @@ repositories {
     mavenCentral()
 }
 
-val JUNIT_VERSION = "5.9.2"
-
 dependencies {
     implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("ch.qos.logback:logback-classic:1.4.6")
     implementation("com.google.guava:guava:31.1-jre")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${JUNIT_VERSION}")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:${JUNIT_VERSION}")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${JUNIT_VERSION}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
     testImplementation("org.mockito:mockito-core:5.2.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
 }
