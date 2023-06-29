931700b6e06af01504f5a8e28ea1ec315c5558c8
b0n541 <j@nschaefer.net>
2023-06-29 23:41:59 +0200

    #157 Add new deep learning player

--- jskat-base/build.gradle.kts
+++ jskat-base/build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
     id("jskat.kotlin-library-conventions")
 }
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.5.2")
+    implementation("org.jetbrains.kotlinx:dataframe:0.11.0")
+}
