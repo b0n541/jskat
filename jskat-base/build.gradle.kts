1038a69afd655e79ad5aa879ccbb2bec22181d03
b0n541 <j@nschaefer.net>
2024-04-01 21:32:29 +0200

    #157 Update Deep Java Library

--- jskat-base/build.gradle.kts
+++ jskat-base/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 dependencies {
-    implementation(platform("ai.djl:bom:0.26.0"))
+    implementation(platform("ai.djl:bom:0.27.0"))
     implementation("ai.djl:api")
     implementation("ai.djl:model-zoo")
     implementation("ai.djl:basicdataset")
